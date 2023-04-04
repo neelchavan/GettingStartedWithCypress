4d47da004b7ca2da9ccf2f56b6f423d3a43c2da4
neelchavan <neelchavan9@gmail.com>
2023-04-04 16:22:45 +0530

    getting data from json and reusing it for multiple iterations

--- cypress/integration/examples/customCommand.js
+++ cypress/integration/examples/customCommand.js
@@ -1,12 +1,23 @@
 /// <reference types="Cypress" />
-describe("data driven test", () => {
-  it("first data driven test using cypress custom commands", () => {
+
+describe("Data driven test", () => {
+  //var productNames;
+  before(function () {
+    cy.fixture("example").then(function (data) {
+      this.data = data;
+    });
+  });
+
+  it("first data driven test using cypress custom commands", function () {
     // open the url
     cy.visit("https://rahulshettyacademy.com/angularpractice/");
     // go to the shop page
     cy.contains("Shop").click();
-    // by product name 'Blackberry' click on the add to cart button.
-    // reffer command.js for below method.
-    cy.addProductToCartByName("Blackberry");
+
+    // based on product name click on the add to cart button.
+    // reffer command.js for below method 'addProductToCartByName'
+    this.data.productNames.forEach((ele) => {
+      cy.addProductToCartByName(ele);
+    });
   });
 });
